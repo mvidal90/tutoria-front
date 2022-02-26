711c904f6f6f77c55e6fa2587e463f9d79e39d61
Marcos Vidal <marcosvidal@MBP-de-Marcos.fibertel.com.ar>
2022-02-26 17:01:07 -0300

    Clase 2

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let activeSection = "Historic";
+
+const selectSection = (section) => {
+    activeSection = section;
+
+    switch (activeSection) {
+        case "Historic":
+            
+            break;
+
+        case "Filters":
+            
+            break;
+        case "Panel":
+            
+            break;
+    
+        default:
+            
+            break;
+    }
+}
