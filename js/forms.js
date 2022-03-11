5a8252e072205d2075066500a0891ec1d0807010
camoreyra <cecilia.moreyra77@gmail.com>
2022-03-11 19:40:13 -0300

    Creación de carpetas de js y css, lectura de los json de pathogenic, incidental y todos, continuación de historia clínica, agregado de tablas en variants-filtred, agregado de código para input editable de últimas columnas e identificación de variantes candidata y/o relacionadas con el fenotipo

---
+++ js/forms.js
@@ -0,0 +1,23 @@
+function ocultarElemento(elemento) {
+
+    const x = document.getElementById(elemento);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+function mostrarValorFrecuencia(){
+  const valorFrecuencia = document.getElementById("frecuenciaPoblacional").value;
+  document.getElementById("valorFrecuencia").innerHTML = Number.parseFloat(valorFrecuencia).toFixed(2);
+}
+
+function filtarPorPredictoresBionformaticos(){}
+
+function filtarPorImpactoFuncional(){}
+
+function filtrarPorTranscripto(){
+  document.getElementById(colTrans).innerHTML = document.getElementById("transcriptoNro").value;
+}
--- js/index.js
+++ js/index.js
@@ -1,122 +1,3 @@
-
-const transcript = 5;
-const primaryKeys = [ "CHROM", "POS", "REF", "ALT", "GT", "AD", "DP", "ID", "GENE", "HGVS_C", "HGVS_P", "FEATUREID"];
-let rows;
-
-const openTab = (evt, tabName) => {
-  // Declare all variables
-  let i, tabcontent, tablinks;
-  const xhttp = new XMLHttpRequest();
-  const urlGenesFenotipo = "/json/genesFenotipo.json";
-  xhttp.responseType = 'json';
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-
-  xhttp.onreadystatechange = () => {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
-      console.log(xhttp.response)
-      const jsonResponse = 
-        xhttp.response.map( obj => ({
-          ...obj,
-          "GENE": obj["GENE"].split(",")[transcript], 
-          "HGVS_C": obj["HGVS_C"].split(",")[transcript], 
-          "HGVS_P": obj["HGVS_P"].split(",")[transcript], 
-          "FEATUREID": obj["FEATUREID"].split(",")[transcript], 
-          "EFFECT": obj["EFFECT"].split(",")[transcript], 
-          "IMPACT": obj["IMPACT"].split(",")[transcript], 
-          "HDIV_pred": obj["HDIV_pred"].split(",")[transcript], 
-          "HVAR_pred": obj["HVAR_pred"].split(",")[transcript], 
-          "SIFT_pred": obj["SIFT_pred"].split(",")[transcript], 
-          "Taster_pred": obj["Taster_pred"].split(",")[transcript],
-          "Assessor_pred": obj["Assessor_pred"].split(",")[transcript],
-          "ExAC_AF": obj["ExAC_AF"] ? parseFloat(obj["ExAC_AF"]).toFixed(4) : "",
-        }));
-
-      rows = jsonResponse;
-      renderTable(jsonResponse);
-    }
-  };
-
-  if (tabName == 'genesFenotipo') {
-    xhttp.open("GET", urlGenesFenotipo, true);
-    xhttp.send();
-  }
-}
-
-const renderTable = (jsonArray) => {
-
-  // Crea un elemento <table> y un elemento <tbody>
-  const tabla = document.getElementById("genesFenTable");
-  const tblBody = document.createElement("tbody");
-  // Crea las celdas
-  for (let i = 0; i < jsonArray.length; i++) {
-    // Crea las hileras de la tabla
-    const hilera = document.createElement("tr");
-    const objGen = jsonArray[i];
-
-    Object.entries(objGen).forEach(([key, value]) => {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
-      const celda = document.createElement("td");
-
-      if (primaryKeys.includes(key)) {
-        celda.id = `celda-key-${key}-${i}`;
-      }
-      // console.log( "Value "+value )
-      const textoCelda = document.createTextNode(value || "");
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
-    });
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
-    tblBody.appendChild(hilera);
-  }
-
-  // posiciona el <tbody> debajo del elemento <table>
-  tabla.appendChild(tblBody);
-  // modifica el atributo "border" de la tabla y lo fija a "2";
-  tabla.setAttribute("border", "2");
-}
-
-const showIDs = () => {
-  const textHead = "id";
-  const textTr = "celda-key-";
-  for (let i = 0; i < primaryKeys.length; i++) {
-    const head = document.getElementById(textHead.concat(i.toString()));
-    if (head.style.display === "none") {
-      head.style.display = "table-cell";
-    } else {
-      head.style.display = "none"
-    }
-  }
-  
-  for (let i = 0; i < rows.length; i++) {
-    for (let k = 0; k < primaryKeys.length; k++) {
-      const celda = document.getElementById(`${textTr}${primaryKeys[k]}-${i}`);
-      if (celda.style.display === "none") {
-        celda.style.display = "table-cell";
-      } else {
-        celda.style.display = "none"
-      }
-    }
-  }
-
-}
-
 const selectSection = (section) => {
   switch (section) {
     case "Historic":
@@ -127,12 +8,20 @@ const selectSection = (section) => {
       document.getElementById("main-section").src = "/pages/variants-filtred.html";
       return;
 
+    case "InformeRes":
+      document.getElementById("main-section").src = "/pages/informeResultados.html";
+    return;
+
+    case "InformeCli":
+      document.getElementById("main-section").src = "/pages/informeClinico.html";
+    return;  
+   
     case "Panel":
       document.getElementById("main-section").src = "/pages/panel-bioinformatico.html";
-      return;
-
+      return;  
+    
     default:
-      document.getElementById("main-section").src = "/pages/historia-clinica.html";
+      document.getElementById("main-section").src = "/pages/presentacion.html";
       return; 
   }
 }
